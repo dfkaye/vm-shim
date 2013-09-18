30d065db3c03468ecdac97014e3d54781c2c0847
david kaye <dfkaye@yahoo.com>
2013-09-18 15:03:16 -0700

    Update vm-shim.js

--- js/vm-shim.js
+++ js/vm-shim.js
@@ -1,6 +1,38 @@
 (function(){
 
-
+  var global = global || window;
+  
+  console.log(global);
+  
   console.log('vm-shim.js');
+
+  function runInNewContext(code, context, filename) {
+    
+    var names = [];
+    var values = [];
+    
+    Object.keys(context).forEach(function(key, i, keys){
+      names.push(key);
+      values.push(context[key]);
+    });
+    
+    typeof code == 'string' || (code = '(' + code.toString() + '())');
+    
+    code = '(function t(' + names.join(',') + '){' + code + '}(' + values.join(',') + '))';
+    
+    //var fn = Function(code);
+    
+    //console.log(fn.toString());
+    
+  }
+  
+  global.vm = {
+    runInNewContext : runInNewContext
+  }
   
 }());
+
+vm.runInNewContext(function(){
+  console.log('code');
+  console.log(info);
+}, { info: 'some info'})
