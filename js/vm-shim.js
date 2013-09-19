701ca8d4ffabc2633615d08f8829d903d5638664
david kaye <dfkaye@yahoo.com>
2013-09-19 10:18:34 -0700

    fixed object/context - context is exposed, however, so clean that up

--- js/vm-shim.js
+++ js/vm-shim.js
@@ -3,45 +3,28 @@
   var global = global || window;
   
   console.log(global);
-  
   console.log('vm-shim.js');
 
-  function runInNewContext(code, context, filename) {
-    
-    var names = [];
-    var values = [];
-    var value;
-    
-    Object.keys(context).forEach(function(key, i, keys){
-      names.push(key);
-      
-      i < 1 || (values += ', ');
-      //values += 'context["' + key + '"]';
-      
-      value = context[key];
-      
-      // more work to do on value type -
-      // string - value
-      // not object, not string - "'" + value + "'"
-      // object not null
-      
-      console.log(value);
-      values.push(typeof value != 'object' && "'" + value + "'" || value);
-    });
+  function runInNewContext(src, context, filename) {
 
-    console.log(values)
-    typeof code == 'string' || (code = '(' + code.toString() + '())');
+    typeof src == 'string' || (src = '(' + src.toString() + '())');
   
-    code = '(function t(context, ' + names.join(',') + '){\n' + code + '\n}(' ;
-    code += values.join(',');
-    code += '))';
-   
+    var code = '(function t(context){\n';
+    (function() {
+      var vars = ';\n';
+      Object.keys(context).forEach(function(key){
+        vars += 'var ' + key + ' = context.' + key + ';\n';
+      });
+      code += vars;
+    }());
+    code += src + '\n}(context))' ;
+    //code += values.join(',');
+    //code += '))';
     console.log(code.toString());
    
-    //var fn = Function(code);
-    
+    var fn = Function('context', code);
     //console.log(fn.toString());
-    //fn();
+    fn(context);
   }
   
   global.vm = {
@@ -49,12 +32,3 @@
   }
   
 }());
-
-vm.runInNewContext("console.log('string'); console.log(string);", { string: 'string value'});
-vm.runInNewContext("console.log('number'); console.log(number);", { number: 347.347});
-vm.runInNewContext("console.log('boolean'); console.log(boolean);", { boolean: true});
-
-vm.runInNewContext(function(){
-  console.log('function');
-  console.log(object);
-}, { object: { id: 'an object'} });
