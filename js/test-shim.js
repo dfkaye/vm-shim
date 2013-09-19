6f597993a0d1f4dfb67d1e6ea43591a95d16ff94
david kaye <dfkaye@yahoo.com>
2013-09-19 10:33:37 -0700

    whitespace, line breaks, etc.

--- js/test-shim.js
+++ js/test-shim.js
@@ -2,14 +2,19 @@
 
   console.log('test-shim');
   
-  vm.runInNewContext("console.log('should be null: ' + context); console.log('string'); console.log(string);", { string: 'string value'});
-  vm.runInNewContext("console.log('should be null: ' + context); console.log('number'); console.log(number);", { number: 347.347});
-  vm.runInNewContext("console.log('should be null: ' + context); console.log('boolean'); console.log(boolean);", { boolean: true});
+  vm.runInNewContext("console.log('should be null: ' + context); console.log('string'); console.log(string);", 
+    { string: 'string value' });
+    
+  vm.runInNewContext("console.log('should be null: ' + context); console.log('number'); console.log(number);", 
+    { number: 347.347 });
+    
+  vm.runInNewContext("console.log('should be null: ' + context); console.log('boolean'); console.log(boolean);", 
+    { boolean: true });
   
   vm.runInNewContext(function(){
     console.log('should be null: ' + context); 
     console.log('function');
     console.log('should be an id/string: ' + object.id);
-  }, { object: { id: 'an id/string'} });
+  }, { object: { id: 'an id/string' } });
 
 }());
