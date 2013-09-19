bbdb457247e7e7b093de01482a5be04c00d0971e
david kaye <dfkaye@yahoo.com>
2013-09-19 10:27:39 -0700

    fix context leakage

--- js/test-shim.js
+++ js/test-shim.js
@@ -2,12 +2,12 @@
 
   console.log('test-shim');
   
-  vm.runInNewContext("console.log(context); console.log('string'); console.log(string);", { string: 'string value'});
-  vm.runInNewContext("console.log(context); console.log('number'); console.log(number);", { number: 347.347});
-  vm.runInNewContext("console.log(context); console.log('boolean'); console.log(boolean);", { boolean: true});
+  vm.runInNewContext("console.log('should be null: ' + context); console.log('string'); console.log(string);", { string: 'string value'});
+  vm.runInNewContext("console.log('should be null: ' + context); console.log('number'); console.log(number);", { number: 347.347});
+  vm.runInNewContext("console.log('should be null: ' + context); console.log('boolean'); console.log(boolean);", { boolean: true});
   
   vm.runInNewContext(function(){
-    console.log(context); 
+    console.log('should be null: ' + context); 
     console.log('function');
     console.log('should be an id/string: ' + object.id);
   }, { object: { id: 'an id/string'} });
