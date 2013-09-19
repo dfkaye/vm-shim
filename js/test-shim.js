b055469fe55db539e0a6bc075a4b2d057746480f
david kaye <dfkaye@yahoo.com>
2013-09-19 10:19:20 -0700

    moved inline vm tests

--- js/test-shim.js
+++ js/test-shim.js
@@ -2,4 +2,14 @@
 
   console.log('test-shim');
   
+  vm.runInNewContext("console.log(context); console.log('string'); console.log(string);", { string: 'string value'});
+  vm.runInNewContext("console.log(context); console.log('number'); console.log(number);", { number: 347.347});
+  vm.runInNewContext("console.log(context); console.log('boolean'); console.log(boolean);", { boolean: true});
+  
+  vm.runInNewContext(function(){
+    console.log(context); 
+    console.log('function');
+    console.log('should be an id/string: ' + object.id);
+  }, { object: { id: 'an id/string'} });
+
 }());
