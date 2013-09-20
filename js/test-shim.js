9c137c6d8239cb0cf6302a9f382ff72a17ae6344
david kaye <dfkaye@yahoo.com>
2013-09-20 10:35:33 -0700

    just runInContext, not runInNewContext

--- js/test-shim.js
+++ js/test-shim.js
@@ -2,16 +2,16 @@
 
   console.log('test-shim');
   
-  vm.runInNewContext("console.log('should be null: ' + context); console.log('string'); console.log(string);", 
+  vm.runInContext("console.log('should be null: ' + context); console.log('string'); console.log(string);", 
     { string: 'string value' });
     
-  vm.runInNewContext("console.log('should be null: ' + context); console.log('number'); console.log(number);", 
+  vm.runInContext("console.log('should be null: ' + context); console.log('number'); console.log(number);", 
     { number: 347.347 });
     
-  vm.runInNewContext("console.log('should be null: ' + context); console.log('boolean'); console.log(boolean);", 
+  vm.runInContext("console.log('should be null: ' + context); console.log('boolean'); console.log(boolean);", 
     { boolean: true });
   
-  vm.runInNewContext(function(){
+  vm.runInContext(function(){
     console.log('should be null: ' + context); 
     console.log('function');
     console.log('should be an id/string: ' + object.id);
