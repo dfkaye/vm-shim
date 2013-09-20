9a33a639c89a92b973554f283420a0e08de09f17
david kaye <dfkaye@yahoo.com>
2013-09-20 10:34:44 -0700

    just runInContext, not runInNewContext

--- js/vm-shim.js
+++ js/vm-shim.js
@@ -4,8 +4,18 @@
   
   console.log(global);
   console.log('vm-shim.js');
+  
+  // Object.keys shim (IE < 9, etc.) copied from:
+  // http://tokenposts.blogspot.com.au/2012/04/javascript-objectkeys-browser.html
+  if (!Object.keys) Object.keys = function(o) {
+    if (o !== Object(o))
+      throw new TypeError('Object.keys called on a non-object');
+    var k=[],p;
+    for (p in o) if (Object.prototype.hasOwnProperty.call(o,p)) k.push(p);
+    return k;
+  }
 
-  function runInNewContext(src, context, filename) {
+  function runInContext(src, context, filename) {
 
     typeof src == 'string' || (src = '(' + src.toString() + '())');
   
@@ -29,7 +39,7 @@
   }
   
   global.vm = {
-    runInNewContext : runInNewContext
+    runInContext : runInContext
   }
   
 }());
